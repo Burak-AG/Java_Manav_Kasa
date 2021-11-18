10a0f6ef70b97ae651bd7a3f2b26a641d98629b7
BurakAG <brklpgonul@gmail.com>
2021-11-18 21:59:04 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,26 @@ import java.util.Scanner;
 // Manav-Kasa Programı
 public class Main {
     public static void main(String[] args) {
+        double armutFiyat = 2.14;
+        double elmaFiyat = 3.67;
+        double domatesFiyat = 1.11;
+        double muzFiyat = 0.95;
+        double patlicanFiyat = 5.00;
+        Scanner input = new Scanner(System.in);
 
+        System.out.print("Armut kaç kilo aldın? : ");
+        int armutKilo = input.nextInt();
+        System.out.print("Elma kaç kilo aldın? : ");
+        int elmaKilo = input.nextInt();
+        System.out.print("Domates kaç kilo aldın? : ");
+        int domatesKilo = input.nextInt();
+        System.out.print("Muz kaç kilo aldın? : ");
+        int muzKilo = input.nextInt();
+        System.out.print("Patlıcan kaç kilo aldın? : ");
+        int patlicanKilo = input.nextInt();
+
+        double toplam = (armutFiyat * armutKilo) + (elmaFiyat * elmaKilo) + (domatesFiyat * domatesKilo) +
+                (muzFiyat * muzKilo) + (patlicanFiyat * patlicanKilo);
+        System.out.println("Toplam tutar : " + toplam + " TL");
     }
 }
